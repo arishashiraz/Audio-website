724e27eb6e827587fc3e4b2044caf8a26e6244b4
Arisha Shiraz <arishashiraz06@gmail.com>
2026-01-27 18:56:48 +0530

    Prepare backend for deployment

--- backend/index.js
+++ backend/index.js
@@ -39,7 +39,10 @@ app.post("/compress", upload.single("audio"), (req, res) => {
   });
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
+
+
